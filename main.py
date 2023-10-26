2bd40d9d7bb33c7bea98c7fce3a332909d1bec68
Samuel DSouza <sambdsouza05@gmail.com>
2023-10-25 21:09:32 -0400

    Added decode and main functions

--- main.py
+++ main.py
@@ -9,9 +9,31 @@ def encode(password):
     return new_password
 
 
-def main():
-    password = input()
-    print(encode(password))
+def decode(password):
+    out = ""
+    for s in password:
+        out += str(int(s) + 3)[-1]
+    return out
 
+
+def main():
+    while True:
+        print("Menu")
+        print("-" * 13)
+        print("1. Encode\n2. Decode \n3. Quit\n")
+        user_input = int(input("Please enter and option: "))
+        password = input("Please enter your password to encode: ")
+        print("Your password has been encoded and stored!")
+
+        if user_input == 1:
+            print("Your password has been encoded and stored!")
+            password = encode(password)
+
+        if user_input == 2:
+            print(f"The encoded password is {password}, and the original password is {decode(password)}.")
+
+        if user_input == 3:
+            return
+            
 
 main()
